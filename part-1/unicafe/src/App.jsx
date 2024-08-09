ec1a14960c8a56c54df7b135961d1b087a42eac2
Juan David Prieto Velasquez <david2005prieto@gmail.com>
2024-08-09 14:02:39 -0500

    Exercise 1.6 of part 1 completed

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -1,10 +1,48 @@
-import "./App.css";
+/* eslint-disable react/prop-types */
+import { useState } from "react";
 
-function App() {
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const FeedbackForm = ({ handleGood, handleNeutral, HandleBad }) => {
   return (
     <>
-      <p>Unicafe</p>
+      <h2>Give feedback</h2>
+      <Button text="Good" handleClick={handleGood} />
+      <Button text="Neutral" handleClick={handleNeutral} />
+      <Button text="Bad" handleClick={HandleBad} />
     </>
+  );
+};
+
+const DisplayFeedback = ({ good, neutral, bad }) => {
+  return (
+    <>
+      <h2>Statistics</h2>
+      <ul>
+        <li>Good - {good}</li>
+        <li>Neutral - {neutral}</li>
+        <li>Bad - {bad}</li>
+      </ul>
+    </>
+  );
+};
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  return (
+    <div>
+      <h1>UNICAFE</h1>
+      <FeedbackForm
+        handleGood={() => setGood(good + 1)}
+        handleNeutral={() => setNeutral(neutral + 1)}
+        HandleBad={() => setBad(bad + 1)}
+      />
+      <DisplayFeedback good={good} neutral={neutral} bad={bad} />
+    </div>
   );
 }
 
